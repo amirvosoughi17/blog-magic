750c4170f849025b27136bea0188851e2bd0e097
amir <amir11.vosoughi@gmail.com>
2023-11-17 14:23:09 +0330

    register - client 100%

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import './globals.css'
+import Navbar from './components/Navbar'
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className='bg-white text-slate-800 mt-[50px]'>
+        <Navbar />
+        {children}
+        </body>
     </html>
   )
 }
