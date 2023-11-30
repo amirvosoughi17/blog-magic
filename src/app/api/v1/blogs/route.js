fd334ee3e3729884d687847377ecbd08be259dfe
EhsanMasoumipor <emasoumipor@gmail.com>
2023-11-30 09:58:36 +0330

    get All blog implemented

--- src/app/api/v1/blogs/route.js
+++ src/app/api/v1/blogs/route.js
@@ -1,8 +1,9 @@
 import { connect } from "@/configs/database";
 import Blog from "@/models/blog.model";
+import User from "@/models/user.model";
 import { jwtDecode } from "jwt-decode";
 connect();
-
+// Create a New Blog
 export async function POST(request) {
     const body = await request.json();
     const formData = body.formData;
@@ -23,16 +24,32 @@ export async function POST(request) {
     // ***     Recive Authenticated user ID (current user)  for put in author        ***
     const token = request.cookies.get('access_token') //   get setted cookie 
     const user = jwtDecode(token?.value) // decode the access_token cookie value for recive user id
-    const userId = user.id
-
+    const user_id = await User.findById(user.id)
+    const author_email = user_id.email
     try {
         const new_blog = await Blog.create({
             title,
             description,
             category,
-            author: userId,
+            author: author_email,
         })
         return Response.json({ message: "your blog created successfully!", data: new_blog }, { status: 201 });
+    } catch (error) {
+        return Response.json({ message: error.message }, { status: 500 });
+    }
+}
+
+
+// Get All created Blogs
+
+export async function GET() {
+    try {
+        const blogs = await Blog.find();
+        const blog_count = await Blog.countDocuments();
+        if (blog_count === 0) {
+            return Response.json({ message: "No Blog Created, Create one" })
+        }
+        return Response.json({ blogs, count: blog_count }, { status: 200 });
     } catch (error) {
         return Response.json({ message: error.message }, { status: 500 });
     }
--- src/app/components/BlogCard.jsx
+++ src/app/components/BlogCard.jsx
@@ -3,33 +3,33 @@ import oImage from '../../../public/office.jpg'
 import CardButton from './CardButton'
 import Link from 'next/link';
 
-const BlogCard = ({blog}) => {
+const BlogCard = ({ blog }) => {
   const formatTimestamp = (timestamp) => {
     const options = {
-      year : "numeric",
-      month : "2-digit",
-      day  : "2-digit",
-      hour12  : true
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour12: true
     }
     const date = new Date(timestamp);
-    const formattedDate = date.toLocaleString("en-Us" , options);
+    const formattedDate = date.toLocaleString("en-Us", options);
     return formattedDate
   }
 
   return (
     <div className="w-[320px] h-[430px] bg-stone-800 text-white">
-        <Image src={oImage} width={320} height={200} alt="blog image" className=" object-cover" />
-        <div className="flex flex-col items-center px-4 py-4 gap-4">
-              <div className="flex flex-col items-center  gap-3">
-                <h1 className="text-lg font-bold text-white">{blog.title}</h1>
-                <p className="text-gray-300  text-sm">{blog.description}</p>
-              </div>
-              <span className="border-t-[1px] w-full border-gray-500/60"></span>
-              <div className="flex items-center justify-between w-full">
-                <CardButton  /> 
-                <p className="text-sm text-gray-100">Created by</p>
-              </div>
+      <Image src={oImage} width={320} height={200} alt="blog image" className=" object-cover" />
+      <div className="flex flex-col items-center px-4 py-4 gap-4">
+        <div className="flex flex-col items-center  gap-3">
+          <h1 className="text-lg font-bold text-white">{blog.title}</h1>
+          <p className="text-gray-300  text-sm">{blog.description}</p>
         </div>
+        <span className="border-t-[1px] w-full border-gray-500/60"></span>
+        <div className="flex items-center justify-between w-full">
+          <CardButton />
+          <p className="text-sm text-gray-100">Created by:{blog.author}</p>
+        </div>
+      </div>
     </div>
   )
 }
